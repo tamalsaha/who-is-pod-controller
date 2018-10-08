884efd69bd505b7319abcda5edbdfa56d93a5681
Tamal Saha <tamal@appscode.com>
2018-10-08 06:40:18 -0700

    added-all

--- main.go
+++ main.go
@@ -1,30 +1,21 @@
 package main
 
 import (
-	"encoding/json"
-	"github.com/evanphx/json-patch"
-	api "github.com/kubedb/apimachinery/apis/kubedb/v1alpha1"
-	"bytes"
-	"context"
 	"fmt"
-	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
-	"k8s.io/apimachinery/pkg/types"
 	"path/filepath"
-	kerr "k8s.io/apimachinery/pkg/api/errors"
+	"strings"
+
 	"github.com/appscode/go/log"
-	admreg_util "github.com/appscode/kutil/admissionregistration/v1beta1"
-	"github.com/appscode/kutil/discovery"
-	watchtools "github.com/appscode/kutil/tools/watch"
-	"k8s.io/api/admissionregistration/v1beta1"
+	"github.com/golang/glog"
+	"k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/meta"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/apimachinery/pkg/fields"
-	"k8s.io/apimachinery/pkg/runtime"
-	"k8s.io/apimachinery/pkg/watch"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	"k8s.io/client-go/discovery"
 	"k8s.io/client-go/dynamic"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
-	"k8s.io/client-go/tools/cache"
 	"k8s.io/client-go/tools/clientcmd"
 	"k8s.io/client-go/util/homedir"
 )
@@ -37,190 +28,96 @@ func main() {
 	if err != nil {
 		log.Fatalf("Could not get Kubernetes config: %s", err)
 	}
-	err = UpdateValidatingWebhookCABundle(config, "validators.kubedb.com")
+
+	w, err := DetectWorkload(config, v1.SchemeGroupVersion.WithResource("pods"), "voyager-operator-7f445b7c94-sc9vg", "kube-system")
 	if err != nil {
-		log.Fatalf("unable to get token for service account: %v", err)
+		log.Fatal(err)
 	}
-
-	d := WebhookXray{
-		config: config,
-		testObj: &api.Postgres{
-			TypeMeta: metav1.TypeMeta{
-				APIVersion: api.SchemeGroupVersion.String(),
-				Kind:       api.ResourceKindPostgres,
-			},
-			ObjectMeta: metav1.ObjectMeta{
-				Namespace: "default",
-				Name:      "webhook-activation-detection",
-			},
-			Spec: api.PostgresSpec{
-				Version: "9.6-v1",
-				TerminationPolicy: api.TerminationPolicyWipeOut,
-
-
-			},
-		},
-		op: v1beta1.Create,
-	}
-	d.IsActive()
-
-	fmt.Println("DONE")
-
-	w := WebhookXray{}
-
-	var x2 PostconditionFunc = w.IsActive
+	fmt.Println(w.GetName())
 }
 
-/*
-Create -> FAIL
-
-Create , PATCH, DELETE
-
-
-*/
-
-// PreconditionFunc returns true if the condition has been reached, false if it has not been reached yet,
-// or an error if the condition failed or detected an error state.
-type PostconditionFunc func() (bool, error)
-
-
-type WebhookXray struct {
-	config *rest.Config
-
-	testObj   runtime.Object
-	op        v1beta1.OperationType
-	transform func(_ runtime.Object)
-}
-
-func (d WebhookXray) IsActive() (bool, error) {
-	kc := kubernetes.NewForConfigOrDie(d.config)
-	dc, _ := dynamic.NewForConfig(d.config)
-
-	gvk := d.testObj.GetObjectKind().GroupVersionKind()
-	fmt.Println("GVK = ", gvk)
-
-	gvr, _ := discovery.ResourceForGVK(kc.Discovery(), gvk)
-	fmt.Println("GVR = ", gvr)
-
-	accessor, _ := meta.Accessor(d.testObj)
-
-	var ri dynamic.ResourceInterface
-
-	if accessor.GetNamespace() != "" {
-		ri = dc.Resource(gvr).Namespace(accessor.GetNamespace())
-	} else {
-		dc.Resource(gvr)
-	}
-
-	var buf bytes.Buffer
-	unstructured.UnstructuredJSONScheme.Encode(d.testObj, &buf)
-
-	u := unstructured.Unstructured{}
-	unstructured.UnstructuredJSONScheme.Decode(buf.Bytes(), nil, &u)
-
-	if d.op == v1beta1.Create {
-		_, err := ri.Create(&u)
-		fmt.Println(err)
-
-		if kerr.IsForbidden(err) {
-			// admission webhook "postgres.validators.kubedb.com" denied the request: spec.replicas "<nil>" invalid. Value must be greater than zero
-			fmt.Println(kerr.ReasonForError(err))
-			fmt.Println(err.Error())
-		} else if err == nil {
-			_ = ri.Delete(accessor.GetName(), &metav1.DeleteOptions{})
-		} else {
-			// some other error
-		}
-
-	} else if d.op == v1beta1.Update {
-		_, err := ri.Create(&u)
-		fmt.Println(err)
-
-		mod := d.testObj.DeepCopyObject()
-		d.transform(mod)
-		modJson, err := json.Marshal(mod)
-
-		patch, err := jsonpatch.CreateMergePatch(buf.Bytes(), modJson)
-		if err != nil {
-			return false, err
-
-		}
-
-		_, err = ri.Patch(accessor.GetName(), types.MergePatchType, patch)
-
-		if kerr.IsForbidden(err) {
-			// admission webhook "postgres.validators.kubedb.com" denied the request: spec.replicas "<nil>" invalid. Value must be greater than zero
-			fmt.Println(kerr.ReasonForError(err))
-			fmt.Println(err.Error())
-		} else if err == nil {
-		} else {
-			// some other error
-		}
-
-		_ = ri.Delete(accessor.GetName(), &metav1.DeleteOptions{})
-	} else if d.op == v1beta1.Delete {
-		_, err := ri.Create(&u)
-		fmt.Println(err)
-
-		err = ri.Delete(accessor.GetName(), &metav1.DeleteOptions{})
-		if kerr.IsForbidden(err) {
-			// admission webhook "postgres.validators.kubedb.com" denied the request: spec.replicas "<nil>" invalid. Value must be greater than zero
-			fmt.Println(kerr.ReasonForError(err))
-			fmt.Println(err.Error())
-		} else if err == nil {
-		} else {
-			// some other error
-		}
-	}
-
-	return false, nil
-}
-
-func UpdateValidatingWebhookCABundle(config *rest.Config, name string) error {
-	err := rest.LoadTLSFiles(config)
-	if err != nil {
-		return err
-	}
-
+func DetectWorkload(config *rest.Config, resource schema.GroupVersionResource, name, namespace string) (*unstructured.Unstructured, error) {
 	kc := kubernetes.NewForConfigOrDie(config)
-
-	lw := &cache.ListWatch{
-		ListFunc: func(options metav1.ListOptions) (runtime.Object, error) {
-			options.FieldSelector = fields.OneTermEqualSelector("metadata.name", name).String()
-			return kc.AdmissionregistrationV1beta1().ValidatingWebhookConfigurations().List(options)
-		},
-		WatchFunc: func(options metav1.ListOptions) (watch.Interface, error) {
-			options.FieldSelector = fields.OneTermEqualSelector("metadata.name", name).String()
-			return kc.AdmissionregistrationV1beta1().ValidatingWebhookConfigurations().Watch(options)
-		},
+	dc, err := dynamic.NewForConfig(config)
+	if err != nil {
+		return nil, err
 	}
 
-	ctx := context.Background()
-	_, err = watchtools.UntilWithSync(ctx,
-		lw,
-		&v1beta1.ValidatingWebhookConfiguration{},
-		nil,
-		func(event watch.Event) (bool, error) {
-			a, _ := meta.Accessor(event.Object)
-			fmt.Println(event.Type, a.GetName())
+	obj, err := dc.Resource(resource).Namespace(namespace).Get(name, metav1.GetOptions{})
+	if err != nil {
+		return nil, err
+	}
+	return findWorkload(kc, dc, obj)
+}
 
-			switch event.Type {
-			case watch.Deleted:
-				return false, nil
-			case watch.Error:
-				return false, fmt.Errorf("error watching")
-			case watch.Added, watch.Modified:
-				cur := event.Object.(*v1beta1.ValidatingWebhookConfiguration)
-				_, _, err := admreg_util.PatchValidatingWebhookConfiguration(kc, cur, func(in *v1beta1.ValidatingWebhookConfiguration) *v1beta1.ValidatingWebhookConfiguration {
-					for i := range in.Webhooks {
-						in.Webhooks[i].ClientConfig.CABundle = config.CAData
-					}
-					return in
-				})
-				return err == nil, err
-			default:
-				return false, fmt.Errorf("unexpected event type: %v", event.Type)
+func findWorkload(kc kubernetes.Interface, dc dynamic.Interface, obj *unstructured.Unstructured) (*unstructured.Unstructured, error) {
+	m, err := meta.Accessor(obj)
+	if err != nil {
+		return nil, err
+	}
+	for _, ref := range m.GetOwnerReferences() {
+		if ref.Controller != nil && *ref.Controller {
+			gvk := schema.FromAPIVersionAndKind(ref.APIVersion, ref.Kind)
+			gvr, err := ResourceForGVK(kc.Discovery(), gvk)
+			if err != nil {
+				return nil, err
 			}
-		})
-	return err
+			parent, err := dc.Resource(gvr).Namespace(m.GetNamespace()).Get(ref.Name, metav1.GetOptions{})
+			if err != nil {
+				return nil, err
+			}
+			return findWorkload(kc, dc, parent)
+		}
+	}
+	return obj, nil
+}
+
+func ResourceForGVK(client discovery.DiscoveryInterface, input schema.GroupVersionKind) (schema.GroupVersionResource, error) {
+	resourceList, err := client.ServerResourcesForGroupVersion(input.GroupVersion().String())
+	if discovery.IsGroupDiscoveryFailedError(err) {
+		glog.Errorf("Skipping failed API Groups: %v", err)
+	} else if err != nil {
+		return schema.GroupVersionResource{}, err
+	}
+	var resources []schema.GroupVersionResource
+	for _, resource := range resourceList.APIResources {
+		if resource.Kind == input.Kind { // match kind
+			resources = append(resources, input.GroupVersion().WithResource(resource.Name))
+		}
+	}
+	resources = FilterSubResources(resources) // ignore sub-resources
+	if len(resources) == 1 {
+		return resources[0], nil
+	}
+	return schema.GroupVersionResource{}, &AmbiguousResourceError{PartialResource: input, MatchingResources: resources}
+}
+
+func FilterSubResources(resources []schema.GroupVersionResource) []schema.GroupVersionResource {
+	var resFiltered []schema.GroupVersionResource
+	for _, res := range resources {
+		if !strings.ContainsRune(res.Resource, '/') {
+			resFiltered = append(resFiltered, res)
+		}
+	}
+	return resFiltered
+}
+
+// AmbiguousResourceError is returned if the RESTMapper finds multiple matches for a resource
+type AmbiguousResourceError struct {
+	PartialResource schema.GroupVersionKind
+
+	MatchingResources []schema.GroupVersionResource
+	MatchingKinds     []schema.GroupVersionKind
+}
+
+func (e *AmbiguousResourceError) Error() string {
+	switch {
+	case len(e.MatchingKinds) > 0 && len(e.MatchingResources) > 0:
+		return fmt.Sprintf("%v matches multiple resources %v and kinds %v", e.PartialResource, e.MatchingResources, e.MatchingKinds)
+	case len(e.MatchingKinds) > 0:
+		return fmt.Sprintf("%v matches multiple kinds %v", e.PartialResource, e.MatchingKinds)
+	case len(e.MatchingResources) > 0:
+		return fmt.Sprintf("%v matches multiple resources %v", e.PartialResource, e.MatchingResources)
+	}
+	return fmt.Sprintf("%v matches multiple resources or kinds", e.PartialResource)
 }
